bac6d89873c8f2525166b8062cf7ac065dbb3cf8
Krekotun <a.krekotun@gmail.com>
2017-01-17 11:07:52 +0200

    store hot reloading

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux'
-import reducers from './modules'
+import reducers from 'store/modules'
 import thunk from 'redux-thunk'
 
 const store = createStore(
 	reducers,
 	applyMiddleware(thunk)
 )
+
+if (module.hot) {
+	module.hot.accept('store/modules', () => {
+		store.replaceReducer(require('store/modules').default)
+	});
+}
 
 export default store
